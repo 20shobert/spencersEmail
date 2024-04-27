d6c88cc95fe46301510bd5497a25267953884662
20shobert <spencerhobert@mail.weber.edu>
2024-04-27 15:17:30 -0600

    Added Mail database model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Mail(models.Model):
+    #sender =
+    #receivers = 
+    title = models.CharField(max_length=200) #Max length of 200 char
+    content = models.TextField() #Text field cannot be blank, forum can be blank (can save the email to be empty, but cannot send an empty email)
+    sentDate = models.DateTimeField(auto_now_add=True) #Time is saved once it's first created
+    isResponse = models.BooleanField(default=False) #If email is a response to another email, true
+    isArchived = models.BooleanField(default=False) #If archived, true
+    isDeleted = models.BooleanField(default=False) #If deleted, true
+    isUnread = models.BooleanField(default=True) #If unread, true
+    isStarred = models.BooleanField(default=False) #If starred, true
+    isSelected = models.BooleanField(default=False) #If selected, true
+
+    def __str__(self):
+        return str(self.title)
--- base/views.py
+++ base/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render
+from .models import Mail
 
 # Create your views here.
 
-inInbox = [
-    {'id': 1, 'title': 'Hello', 'content': 'What\'s up man?'},
-    {'id': 2, 'title': 'Sup', 'content': 'Sup sir. I heard you\'re in need of assistance'},
-    {'id': 3, 'title': 'Penis', 'content': 'WOOO it\'s penis appreciation month!'},
-]
+# inInbox = [
+#     {'id': 1, 'title': 'Hello', 'content': 'What\'s up man?'},
+#     {'id': 2, 'title': 'Sup', 'content': 'Sup sir. I heard you\'re in need of assistance'},
+#     {'id': 3, 'title': 'Penis', 'content': 'WOOO it\'s penis appreciation month!'},
+# ]
 
 def home(request):
-    context = {'inInbox': inInbox}
+    mail = Mail.objects.all() #Grab all mail in database
+
+    context = {'mail': mail}
 
     return render(request, 'home.html', context)
 
 def mail(request, pk):
-    letter = 0
-    for i in inInbox:
-        if i['id'] == int(pk):
-            letter = i
+    letter = Mail.objects.get(id=pk) #Grab whatever letter is equal to the pk
 
     context = {'letter': letter}
 
