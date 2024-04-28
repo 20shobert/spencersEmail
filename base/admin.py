84fade9bc99becf97790a8ee05059de46afe853a
20shobert <spencerhobert@mail.weber.edu>
2024-04-27 18:45:08 -0600

    Changed Inbox, Archive, Deleted, and Highlighted models to be a cell in Box

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from .models import Box
 from .models import Mail
 
 # Register your models here.
 
+admin.site.register(Box)
 admin.site.register(Mail)
--- base/models.py
+++ base/models.py
@@ -4,44 +4,19 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 #Boxes
-class Inbox(models.Model):
-    numInside = models.IntegerField() #Number of emails inside this box
+class Box(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True) #null=True IS TEMPORARY. CHANGE LATER.
+    name = models.CharField(max_length=11) #Name of box
+    numInside = models.IntegerField(default=0) #Number of emails inside this box
 
     def __str__(self):
-        return self.owner
-    
-class Archive(models.Model): #Mail that's been archived
-    numInside = models.IntegerField() #Number of emails inside this box
-
-    def __str__(self):
-        return self.owner
-    
-class Deleted(models.Model): # Mail that's been deleted
-    numInside = models.IntegerField() #Number of emails inside this box
-
-    def __str__(self):
-        return self.owner
-
-class Highlighted(models.Model): #Mail that's been highlighted
-    numInside = models.IntegerField() #Number of emails inside this box
-
-    def __str__(self):
-        return self.owner
-    
-class Boxes(models.Model): #Collection of all boxes
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True) #Who owns these boxes. #null=True IS TEMPORARY. CHANGE LATER.
-
-    #Collection of boxes
-    inboxBox = models.OneToOneField(Inbox, on_delete=models.CASCADE)
-    archiveBox = models.OneToOneField(Archive, on_delete=models.CASCADE)
-    deletedBox = models.OneToOneField(Deleted, on_delete=models.CASCADE)
-    highlightedBox = models.OneToOneField(Highlighted, on_delete=models.CASCADE)
+        return str(self.name)
 
 
 #Mail
 class Mail(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, null=True) #null=True IS TEMPORARY. CHANGE LATER.
-    #receiver = #Will add multiple receivers later
+    currentBox = models.ForeignKey(Box, on_delete=models.CASCADE, null=True) #Which box the mail is currently in. Defaults to Inbox.
     title = models.CharField(max_length=200) #Max length of 200 char
     content = models.TextField() #Text field cannot be blank
     sentDate = models.DateTimeField(auto_now_add=True) #Time is saved once it's first created
@@ -51,8 +26,6 @@ class Mail(models.Model):
     isUnread = models.BooleanField(default=True) #If unread, true. Defaults to unread
     isHighlighted = models.BooleanField(default=False) #If highlighted, true
     isSelected = models.BooleanField(default=False) #If selected, true
-
-    currentBox = models.ForeignKey(Inbox, on_delete=models.CASCADE, null=True) #Which box the mail is currently in. Defaults to Inbox
 
     def __str__(self):
         return str(self.title)
