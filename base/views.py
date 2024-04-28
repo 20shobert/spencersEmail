88dbda1d1b4947572487a5e42d08641227b12637
20shobert <spencerhobert@mail.weber.edu>
2024-04-27 23:36:10 -0600

    Fixed response form not working issue and added Read/Unread functionality

--- base/views.py
+++ base/views.py
@@ -13,7 +13,8 @@ def home(request): #On the homescreen
 
 def mail(request, pk): #Looking at an email
     letter = Mail.objects.get(id=pk) #Grab whatever letter is equal to the pk
-
+    letter.isUnread = False
+    letter.save()
     context = {'letter': letter}
 
     return render(request, 'mail.html', context)
@@ -25,6 +26,8 @@ def sendMail(request): #Sending an email
         if form.is_valid():
             form.save()
             return redirect('home')
+        else:
+            print(form.errors)
 
     context = {'form': form}
 
@@ -33,7 +36,7 @@ def sendMail(request): #Sending an email
 def respond(request, pk): #Responding to an email
     letter = Mail.objects.get(id=pk) #Grab whatever letter is equal to the pk
     letter.receiver = letter.sender #Sender becomes the receiver
-    letter.sender = None #Will change the sender to the user later. CHANGE LATER.
+    #letter.sender = None #Will change the sender to the user later. CHANGE LATER.
     if letter.previousContent != None: #If response content isn't empty
         letter.previousContent = letter.content + '\n\n---------------------\n\n' + letter.previousContent #Content goes into response content
     else: #If response content IS empty
@@ -48,6 +51,8 @@ def respond(request, pk): #Responding to an email
         if form.is_valid():
             form.save()
             return redirect('home')
+        else:
+            print(form.errors)
 
     context = {'letter': letter, 'form': form}
 
