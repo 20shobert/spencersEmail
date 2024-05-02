d6a9265093a6dff2e953006516bbd6259bf4894f
20shobert <spencerhobert@mail.weber.edu>
2024-05-01 20:47:42 -0600

    Getting off for the night 5/1/24

--- base/forms.py
+++ base/forms.py
@@ -1,10 +1,9 @@
 from django import forms
-from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Mail
 
-class MailForm(ModelForm):
+class MailForm(forms.ModelForm):
     class Meta:
         model = Mail
         fields = '__all__'
